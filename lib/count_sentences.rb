d3327bc0ed96341234426b0d32aed13677165cf9
T-erry <terrywachuka10@gmail.com>
2023-02-10 09:09:52 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+  end_with?(".")   
   end
 
   def question?
-
+  end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
-
+    split(/\?|\.|!/).filter{|w|w!=""}.size
   end
 end
+s1 = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
